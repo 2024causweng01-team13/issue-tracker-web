6aaf0a25cf7898a3c4e4eb688d64779f63b6d944
Chaehwan Lim <chlim428@gmail.com>
2024-05-22 00:31:26 +0900

    feat: vite, ui library, react-query 설정

---
+++ src/apis/index.ts
@@ -0,0 +1,26 @@
+import { QueryCache, QueryClient } from "@tanstack/react-query";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+
+const API_HOST = "http://localhost:8080";
+
+export const fetcher = axios.create({
+  baseURL: API_HOST,
+  withCredentials: true,
+})
+
+const defaultErrorHandler = (err: any) => {
+  console.error(err);
+};
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 5 * 1000,
+    },
+  },
+  queryCache: new QueryCache({
+    onError: defaultErrorHandler,
+  }),
+});
