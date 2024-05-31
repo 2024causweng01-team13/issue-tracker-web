26259133a5762b7c1c5ffbad14bb4f0600088c4d
qkrwlgns99 <86741860+qkrwlgns99@users.noreply.github.com>
2024-06-01 02:13:02 +0900

    Update IssueCard.tsx

--- src/pages/projects/projectId/IssueCard.tsx
+++ src/pages/projects/projectId/IssueCard.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import {Issue} from './IssueInterface';
 
-export interface Issue {
-  id: number;
-  title: string;
-  description: string;
-  reporter?: string;
-  reportedDate?: string;
-  fixer?: string;
-  assignee?: string;
-  priority?: 'Blocker' | 'Critical' | 'Major' | 'Minor' | 'Trivial';
-  status: 'New' | 'Assigned' | 'Resolved' | 'Closed' | 'Reopened';
-  comment?: string;
-}
+// export interface Issue {
+//   id: number;
+//   title: string;
+//   description: string;
+//   reporter: string;
+//   reportedDate?: string;
+//   fixer: string;
+//   assignee: string;
+//   priority: 'Blocker' | 'Critical' | 'Major' | 'Minor' | 'Trivial';
+//   status: 'New' | 'Assigned' | 'Resolved' | 'Closed' | 'Reopened';
+//   comment: string;
+//   keyword:string;
+// }
 
 interface IssueCardProps {
   issue: Issue;
@@ -28,7 +30,7 @@ const IssueCard: React.FC<IssueCardProps> = ({ issue }) => {
   return (
     <div className="issue-card" onClick={handleClick} style={{ cursor: 'pointer' }}>
       <h3>{issue.title}</h3>
-      <p>{issue.description}</p>
+      <p>{issue.keyword}</p>
       <p>Status: {issue.status}</p>
     </div>
   );
