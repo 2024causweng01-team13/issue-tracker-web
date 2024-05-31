87aeb30fbb6ec6d751145490a5f0bb09531d606c
qkrwlgns99 <86741860+qkrwlgns99@users.noreply.github.com>
2024-05-31 18:59:03 +0900

    Update routers.tsx

--- src/routes/routers.tsx
+++ src/routes/routers.tsx
@@ -1,13 +1,12 @@
-import { Home } from "@/pages"
-import { Login } from "@/pages/login"
-import { ProjectsBoard } from "@/pages/projects"
-import { CreateProject } from "@/pages/projects/new"
-import { ProjectDetail } from "@/pages/projects/projectId"
-import { ProjectAnalytics } from "@/pages/projects/projectId/analytics"
-import { IssueDetail } from "@/pages/projects/projectId/issues/issueId"
-import { CreateIssue } from "@/pages/projects/projectId/issues/new"
-import { SignUp } from "@/pages/sign-up"
-import { Route, Routes } from "react-router-dom"
+import { Home } from "@/pages";
+import  {Login}  from "@/pages/login";
+import ProjectMainBoard from "@/pages/projects";
+import  ProjectDetail  from "@/pages/projects/projectId";
+import { ProjectAnalytics } from "@/pages/projects/projectId/analytics";
+import IssueDetail from "@/pages/projects/projectId/issues/issueId";
+import  CreateIssue  from "@/pages/projects/projectId/new";
+import  {SignUp}  from "@/pages/sign-up";
+import { Route, Routes } from "react-router-dom";
 
 export const PATHS = {
   HOME: "/",
@@ -18,8 +17,8 @@ export const PATHS = {
   PROJECT_DETAIL: "/projects/:projectId",
   PROJECT_ANALYTICS: "/projects/:projectId/analytics",
   ISSUE_DETAIL: "/projects/:projectId/issues/:issueId",
-  CREATE_ISSUE: "/projects/:projectId/issues/new",
-}
+  CREATE_ISSUE: "/projects/:projectId/new",
+};
 
 export const Router = () => {
   return (
@@ -27,12 +26,11 @@ export const Router = () => {
       <Route path={PATHS.HOME} element={<Home />} />
       <Route path={PATHS.LOGIN} element={<Login />} />
       <Route path={PATHS.SIGNUP} element={<SignUp />} />
-      <Route path={PATHS.PROJECTS_BOARD} element={<ProjectsBoard />} />
-      <Route path={PATHS.CREATE_PROJECT} element={<CreateProject />} />
+      <Route path={PATHS.PROJECTS_BOARD} element={<ProjectMainBoard />} />
       <Route path={PATHS.PROJECT_DETAIL} element={<ProjectDetail />} />
       <Route path={PATHS.PROJECT_ANALYTICS} element={<ProjectAnalytics />} />
       <Route path={PATHS.ISSUE_DETAIL} element={<IssueDetail />} />
       <Route path={PATHS.CREATE_ISSUE} element={<CreateIssue />} />
     </Routes>
-  )
-}
+  );
+};
