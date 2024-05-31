d82810c481fbcf9803f10d0bf7be586b548b42ba
qkrwlgns99 <86741860+qkrwlgns99@users.noreply.github.com>
2024-06-01 02:12:53 +0900

    Update IssueBoard.tsx

--- src/pages/projects/projectId/IssueBoard.tsx
+++ src/pages/projects/projectId/IssueBoard.tsx
@@ -1,14 +1,32 @@
 import React, { useState } from 'react';
 import { Button, Modal } from 'antd';
-import IssueCard, { Issue } from './IssueCard';
+import IssueCard from './IssueCard';
+import { Issue } from './IssueInterface';
 import CreateIssue from './new';
 import '../../../styles/IssueBoard.css';
 
-const initialIssues: Issue[] = [
-  { id: 1, title: '첫 번째 이슈', description: '이슈 설명', status: 'New' },
-  { id: 2, title: '두 번째 이슈', description: '다른 이슈 설명', status: 'Assigned' },
-  { id: 3, title: '세 번째 이슈', description: '또 다른 이슈 설명', status: 'Resolved' },
-  { id: 4, title: '네 번째 이슈', description: '이슈 설명 업데이트', status: 'Closed' },
+export const initialIssues: Issue[] = [
+  { id: 1, 
+    title: '첫 번째 이슈', 
+    description: '이슈 설명', 
+    reporter: '1', 
+    reportedDate:'2024.05.31',
+    fixer:'11', assignee:'111', 
+    priority:'Blocker', 
+    status: 'New', 
+    comments:[], 
+    keyword:'이슈설명' },
+  { id: 2, 
+    title: '두 번째 이슈', 
+    description: '다른 이슈 설명', 
+    reporter: '2', 
+    reportedDate:'2024.05.31',
+    fixer:'22', 
+    assignee:'222', 
+    priority:'Trivial', 
+    status: 'Reopened', 
+    comments:[], 
+    keyword:'다른이슈 설명', },
 ];
 
 
