778d7534278dcff0f19cac91b498b8b87482234e
qkrwlgns99 <86741860+qkrwlgns99@users.noreply.github.com>
2024-05-31 19:25:55 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,18 @@ import React, { useMemo } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import './App.css';
 import { PATHS, Router } from './routes/routers';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalSyles = createGlobalStyle
+  `
+  html {
+    --grey: #3A3A3A;
+    --midGrey: #666666;
+    --lightGrey: #f2f2f2;
+    --offWhite: #ededed;
+    --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
+  }
+`;
 
 const { Header, Content, Sider } = Layout;
 
@@ -33,20 +45,21 @@ export const App = () => {
   return (
     <div className='max-w-screen-xl m-auto h-full'>
       <Layout style={{ height: '100%', overflow: 'auto' }}>
+        <GlobalSyles />
         <Header style={{ display: 'flex', alignItems: 'center', width: '100%', padding: 30 }}>
           <h2 className="text-[#FFFFFF]">Issue Tracker</h2>
         </Header>
         <Layout>
           <Sider width={200}>
-            <Menu 
-              mode="inline" 
-              items={Menus} 
+            <Menu
+              mode="inline"
+              items={Menus}
               style={{ height: '100%', borderRight: 0 }}
             />
           </Sider>
           <Layout style={{ padding: '0 24px 24px' }}>
             <Breadcrumb style={{ margin: '16px 0' }}
-              items={breadCrumbContent} 
+              items={breadCrumbContent}
             />
             <Content
               style={{
@@ -57,7 +70,7 @@ export const App = () => {
                 borderRadius: borderRadiusLG,
                 overflow: 'auto',
               }}>
-                <Router />
+              <Router />
             </Content>
           </Layout>
         </Layout>
