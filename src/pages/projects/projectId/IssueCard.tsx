53387178a9cc99c0fd54e9c1e9e5b632d012c52e
qkrwlgns99 <86741860+qkrwlgns99@users.noreply.github.com>
2024-06-02 01:48:43 +0900

    Update IssueCard.tsx

--- src/pages/projects/projectId/IssueCard.tsx
+++ src/pages/projects/projectId/IssueCard.tsx
@@ -2,20 +2,6 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import {Issue} from './IssueInterface';
 
-// export interface Issue {
-//   id: number;
-//   title: string;
-//   description: string;
-//   reporter: string;
-//   reportedDate?: string;
-//   fixer: string;
-//   assignee: string;
-//   priority: 'Blocker' | 'Critical' | 'Major' | 'Minor' | 'Trivial';
-//   status: 'New' | 'Assigned' | 'Resolved' | 'Closed' | 'Reopened';
-//   comment: string;
-//   keyword:string;
-// }
-
 interface IssueCardProps {
   issue: Issue;
 }
